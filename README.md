f137aee222d2955f091e877518e9a5101f02b819
sealfury <willspecker@gmail.com>
2021-09-24 14:08:05 +0200

    relocates errors and middlewares into common module syncs exports

---
+++ README.md
@@ -0,0 +1,7 @@
+### A Common Module Created For Tickets Microservices
+
+#### See the [repo](https://github.com/sealfury/tickets-microservices-rehash) for details
+
+#### Link to [NPM Package](https://www.npmjs.com/package/@sealtix/common)
+
+More info coming...
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// ERRORS
+export * from './errors/bad-request-error'
+export * from './errors/custom-error'
+export * from './errors/database-connection-error'
+export * from './errors/not-authorized-error'
+export * from './errors/not-found-error'
+export * from './errors/request-validation-error'
+
+// MIDDLEWARES
+export * from './middlewares/current-user'
+export * from './middlewares/error-handler'
+export * from './middlewares/require-auth'
+export * from './middlewares/validate-request'
